dca715b859f70641212d93377abfaeeec88a450c
Aodhgan <aodhgan@pooltogether.com>
2021-12-14 12:05:08 -0800

    runCLI

--- scripts/runCli.js
+++ scripts/runCli.js
@@ -2,16 +2,18 @@ const findMostRecentDrawCommitedForChainId = require('./helpers/findMostRecentDr
 const getNewestPrizeDistribution = require('./helpers/getNewestPrizeDistribution');
 // const cli = require('@pooltogether/draw-calculator-cli/dist/index.js');
 
-function run() {
+async function run() {
   let chaindId = 1;
+  console.log(`checking if draw calculator CLI needs to be run for chainId: ${chainId}`);
   if (checkIfRunRequired(chaindId));
   {
     console.log(`running CLI for chainId: ${chaindId}`);
   }
 }
+run();
 
-function checkIfRunRequired(chainId) {
+async function checkIfRunRequired(chainId) {
   const mostRecentCommit = findMostRecentDrawCommitedForChainId(chainId);
-  const newestPrizeDistribution = getNewestPrizeDistribution(chainId);
+  const newestPrizeDistribution = await getNewestPrizeDistribution(chainId);
   return mostRecentCommit != newestPrizeDistribution.drawId;
 }
