47c09ed96c2f61bc3e0b2a9e59baf2c65c818eed
Aodhgan <aodhgan@pooltogether.com>
2021-12-14 14:01:11 -0800

    spawning

--- scripts/runCli.js
+++ scripts/runCli.js
@@ -1,13 +1,37 @@
 const findMostRecentDrawCommitedForChainId = require('./helpers/findMostRecentDrawCommitedForChainId');
 const getNewestPrizeDistribution = require('./helpers/getNewestPrizeDistribution');
-// const cli = require('@pooltogether/draw-calculator-cli/dist/index.js');
+const spawnCLIProcess = require('./spawnCLIProcess');
 
 async function run() {
-  let chaindId = 1;
+  let chainId = 1;
   console.log(`checking if draw calculator CLI needs to be run for chainId: ${chainId}`);
-  if (checkIfRunRequired(chaindId));
+  if (await checkIfRunRequired(chainId));
   {
-    console.log(`running CLI for chainId: ${chaindId}`);
+    const newestPrizeDistributionDrawId = (await getNewestPrizeDistribution(chainId)).drawId;
+    console.log(`running CLI for chainId: ${chainId} and drawId ${newestPrizeDistributionDrawId}`);
+
+    await spawnCLIProcess(
+      chainId,
+      '0xdd4d117723C257CEe402285D3aCF218E9A8236E1',
+      newestPrizeDistributionDrawId,
+      './api/prizes',
+    );
+  }
+
+  // now polygon
+  chainId = 137;
+  console.log(`checking if draw calculator CLI needs to be run for chainId: ${chainId}`);
+  if (await checkIfRunRequired(chainId));
+  {
+    const newestPrizeDistributionDrawId = (await getNewestPrizeDistribution(chainId)).drawId;
+    console.log(`running CLI for chainId: ${chainId} and drawId ${newestPrizeDistributionDrawId}`);
+
+    await spawnCLIProcess(
+      chainId,
+      '0x6a304dFdb9f808741244b6bfEe65ca7B3b3A6076',
+      newestPrizeDistributionDrawId,
+      './api/prizes',
+    );
   }
 }
 run();
---
+++ scripts/spawnCLIProcess.js
@@ -0,0 +1,33 @@
+const { spawn } = require('child_process');
+
+async function spawnCLIProcess(chainId, ticket, drawId, directory) {
+  console.log('spawning node process');
+  const child = spawn(
+    'node',
+    [
+      './node_modules/@pooltogether/draw-calculator-cli/dist/index.js',
+      '-c',
+      chainId,
+      '-t',
+      ticket,
+      '-d',
+      drawId,
+      '-o',
+      directory,
+    ],
+    { cwd: process.cwd() },
+  );
+  // use child.stdout.setEncoding('utf8'); if you want text chunks
+  child.stdout.on('data', (chunk) => {
+    // data from standard output is here as buffers
+    console.log(chunk.toString());
+  });
+  child.stderr.on('data', (chunk) => {
+    // data from standard output is here as buffers
+    console.log(chunk.toString());
+  });
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
+module.exports = spawnCLIProcess;
