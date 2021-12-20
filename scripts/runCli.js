69db982f5870d466ab4460bb388d4de712afe463
Aodhgan <aodhgan@pooltogether.com>
2021-12-20 11:23:05 -0800

    attempt to catch up

--- scripts/runCli.js
+++ scripts/runCli.js
@@ -1,6 +1,8 @@
 const spawnCLIProcess = require('./spawnCLIProcess');
 const checkIfCLIRunRequired = require('./helpers/checkIfCLIRunRequired');
 const getNewestPrizeDistribution = require('./helpers/getNewestPrizeDistribution');
+const findMostRecentDrawCommitedForChainId = require('./helpers/findMostRecentDrawCommitedForChainId');
+
 const { MAINNET_TICKET_ADDRESS, POLYGON_TICKET_ADDRESS } = require('./constants');
 
 const core = require('@actions/core');
@@ -11,11 +13,22 @@ async function run() {
 
   if (await checkIfCLIRunRequired(chainId)) {
     const newestPrizeDistributionDrawId = (await getNewestPrizeDistribution(chainId)).drawId;
-    console.log(`running CLI for chainId: ${chainId} and drawId ${newestPrizeDistributionDrawId}`);
-    await spawnCLIProcess(chainId, MAINNET_TICKET_ADDRESS, newestPrizeDistributionDrawId, path);
+    const mostRecentCommitedDrawId = findMostRecentDrawCommitedForChainId(chainId);
+
+    // need to run between these two draw Ids - create a range
+    const draws = Array.from(
+      { length: mostRecentCommitedDrawId - newestPrizeDistributionDrawId + 1 },
+      (v, k) => k + mostRecentCommitedDrawId,
+    );
+    draws.forEach((drawId) => {
+      console.log(
+        `running CLI for chainId: ${chainId} and drawId ${newestPrizeDistributionDrawId}`,
+      );
+      await spawnCLIProcess(chainId, MAINNET_TICKET_ADDRESS, newestPrizeDistributionDrawId, path);
+    });
 
     core.setOutput('mainnetCliToolRan', 'true');
-    core.setOutput('mainnetDrawId', newestPrizeDistributionDrawId);
+    core.setOutput('mainnetDrawId', JSON.stringify(draws));
     core.setOutput('mainnetChainId', chainId);
   }
 
@@ -24,11 +37,22 @@ async function run() {
 
   if (await checkIfCLIRunRequired(chainId)) {
     const newestPrizeDistributionDrawId = (await getNewestPrizeDistribution(chainId)).drawId;
-    console.log(`running CLI for chainId: ${chainId} and drawId ${newestPrizeDistributionDrawId}`);
-    await spawnCLIProcess(chainId, POLYGON_TICKET_ADDRESS, newestPrizeDistributionDrawId, path);
+    const mostRecentCommitedDrawId = findMostRecentDrawCommitedForChainId(chainId);
+
+    // need to run between these two draw Ids - create a range
+    const draws = Array.from(
+      { length: mostRecentCommitedDrawId - newestPrizeDistributionDrawId + 1 },
+      (v, k) => k + mostRecentCommitedDrawId,
+    );
+    draws.forEach((drawId) => {
+      console.log(
+        `running CLI for chainId: ${chainId} and drawId ${newestPrizeDistributionDrawId}`,
+      );
+      await spawnCLIProcess(chainId, POLYGON_TICKET_ADDRESS, newestPrizeDistributionDrawId, path);
+    });
 
     core.setOutput('polygonCliToolRan', 'true');
-    core.setOutput('polygonDrawId', newestPrizeDistributionDrawId);
+    core.setOutput('polygonDrawId', JSON.stringify(draws));
     core.setOutput('polygonChainId', chainId);
   }
 
