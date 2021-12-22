9accbcadfe7c44287c4111b899e50a5bb71df1f5
Aodhgan <aodhgan@pooltogether.com>
2021-12-21 17:04:23 -0800

    add avalanche

--- scripts/runCli.js
+++ scripts/runCli.js
@@ -3,59 +3,57 @@ const checkIfCLIRunRequired = require('./helpers/checkIfCLIRunRequired');
 const getNewestPrizeDistribution = require('./helpers/getNewestPrizeDistribution');
 const findMostRecentDrawCommitedForChainId = require('./helpers/findMostRecentDrawCommitedForChainId');
 
-const { MAINNET_TICKET_ADDRESS, POLYGON_TICKET_ADDRESS } = require('./constants');
+const {
+  MAINNET_TICKET_ADDRESS,
+  POLYGON_TICKET_ADDRESS,
+  AVALANCHE_TICKET_ADDRESS,
+} = require('./constants');
 
 const core = require('@actions/core');
 
 async function run() {
-  const path = './api/prizes';
-  let chainId = 1;
-
-  if (await checkIfCLIRunRequired(chainId)) {
-    const newestPrizeDistributionDrawId = (await getNewestPrizeDistribution(chainId)).drawId;
-    const mostRecentCommitedDrawId = findMostRecentDrawCommitedForChainId(chainId);
-
-    // need to run between these two draw Ids - create a range
-    const draws = Array.from(
-      { length: newestPrizeDistributionDrawId - mostRecentCommitedDrawId + 1 },
-      (v, k) => k + mostRecentCommitedDrawId,
-    );
-    console.log('running CLI for draws: ', draws);
-
-    for (let drawId of draws) {
-      console.log(`running CLI for chainId: ${chainId} and drawId ${drawId}`);
-      await spawnCLIProcess(chainId, MAINNET_TICKET_ADDRESS, drawId, path);
-    }
-
-    core.setOutput('mainnetCliToolRan', 'true');
-    core.setOutput('mainnetDrawId', JSON.stringify(draws));
-    core.setOutput('mainnetChainId', chainId);
-  }
-
-  // now polygon
-  chainId = 137;
-
-  if (await checkIfCLIRunRequired(chainId)) {
-    const newestPrizeDistributionDrawId = (await getNewestPrizeDistribution(chainId)).drawId;
-    const mostRecentCommitedDrawId = findMostRecentDrawCommitedForChainId(chainId);
-
-    // need to run between these two draw Ids - create a range
-    const draws = Array.from(
-      { length: newestPrizeDistributionDrawId - mostRecentCommitedDrawId + 1 },
-      (v, k) => k + mostRecentCommitedDrawId,
-    );
-    console.log('running CLI for draws: ', draws);
-    for (let drawId of draws) {
-      console.log(`running CLI for chainId: ${chainId} and drawId ${drawId}`);
-      await spawnCLIProcess(chainId, POLYGON_TICKET_ADDRESS, drawId, path);
-    }
-
-    core.setOutput('polygonCliToolRan', 'true');
-    core.setOutput('polygonDrawId', JSON.stringify(draws));
-    core.setOutput('polygonChainId', chainId);
-  }
+  await runForChainId(1, 'mainnetCliToolRan', 'mainnetDrawId', 'mainnetChainId');
+  await runForChainId(137, 'polygonCliToolRan', 'polygonDrawId', 'polygonChainId');
+  await runForChainId(43114, 'avalancheCliToolRan', 'avalancheDrawId', 'avalancheChainId');
 
   console.log('done! exiting 0');
   process.exit(0);
 }
 run();
+
+async function runForChainId(chainId, chainIdRunBoolean, chainIdDrawIdMsg, chainIdIdentifier) {
+  const path = './api/prizes';
+
+  if (await checkIfCLIRunRequired(chainId)) {
+    const newestPrizeDistributionDrawId = (await getNewestPrizeDistribution(chainId)).drawId;
+    const mostRecentCommitedDrawId = findMostRecentDrawCommitedForChainId(chainId);
+
+    // need to run between these two draw Ids exclusive of the first - create a range
+    const draws = Array.from(
+      { length: newestPrizeDistributionDrawId - mostRecentCommitedDrawId },
+      (v, k) => k + mostRecentCommitedDrawId + 1,
+    );
+    console.log('running CLI for draws: ', draws);
+    for (let drawId of draws) {
+      console.log(`running CLI for chainId: ${chainId} and drawId ${drawId}`);
+      await spawnCLIProcess(chainId, lookupTicketAddress(chainId), drawId, path);
+    }
+
+    core.setOutput(chainIdRunBoolean, 'true');
+    core.setOutput(chainIdDrawIdMsg, JSON.stringify(draws));
+    core.setOutput(chainIdIdentifier, chainId);
+  }
+}
+
+function lookupTicketAddress(chainId) {
+  switch (chainId) {
+    case 1:
+      return MAINNET_TICKET_ADDRESS;
+    case 137:
+      return POLYGON_TICKET_ADDRESS;
+    case 43114:
+      return AVALANCHE_TICKET_ADDRESS;
+    default:
+      throw new Error(`unsupported chainId: ${chainId}`);
+  }
+}
