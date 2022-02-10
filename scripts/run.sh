f2ff496fa64c19c7d54d50485fb70ec72fda902c
kames <kamescg@gmail.com>
2022-02-10 08:32:03 -0700

    remove(scripts): outdated

--- scripts/run.sh
+++
@@ -1,5 +0,0 @@
-node "$(dirname "$0")/runCli.js" "$input"
-   echo "Script: $? - Successfull"
-if [ $? != 0 ]; then                   
-   echo "${?}\n". 1>&2 && exit 1
-fi
--- test/getNewestPrizeDistribution.test.js
+++
@@ -1,9 +0,0 @@
-const expect = require('chai').expect;
-const getNewestPrizeDistribution = require('../scripts/helpers/getNewestPrizeDistribution');
-
-describe('getNewestPrizeDistribution', () => {
-  it('getNewestPrizeDistribution', async () => {
-    const result = await getNewestPrizeDistribution('1');
-    expect(result.drawId).to.be.greaterThan(1);
-  }).timeout(90000);
-});
